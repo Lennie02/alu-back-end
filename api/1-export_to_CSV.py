efa185e09aa3909b50fdf6bcd127d7c36d8357ad
Lennie02 <o.ishimwe1@alustudent.com>
2026-02-23 01:39:27 +0000

    Export to CVS.

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Export employee TODO list data to CSV format."""
+import csv
+import json
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    with urllib.request.urlopen("{}/users/{}".format(base_url, employee_id)) as response:
+        user = json.loads(response.read().decode("utf-8"))
+
+    with urllib.request.urlopen("{}/todos?userId={}".format(base_url, employee_id)) as response:
+        todos = json.loads(response.read().decode("utf-8"))
+
+    username = user.get("username")
+    filename = "{}.csv".format(employee_id)
+
+    with open(filename, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            writer.writerow([
+                employee_id,
+                username,
+                task.get("completed"),
+                task.get("title")
+            ])
